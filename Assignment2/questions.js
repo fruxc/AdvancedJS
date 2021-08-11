ad7eeaec3b21709ae7dcb94872e087b5abef838a
Hammad Ansari <hammadansari17@gmail.com>
2021-08-11 21:43:25 +0530

    Added screenshots and question 2 for assignment 2 completed

--- Assignment2/questions.js
+++ Assignment2/questions.js
@@ -44,6 +44,7 @@ const MoviesByGenre = async (movies, genre) => {
 }
 
 QuestionOne = async () => {
+    console.time("exec")
     await getResponseData("https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json").then((movies) => {
         let result = {
             "Actors": [],
@@ -81,9 +82,33 @@ QuestionOne = async () => {
     }).catch((error) => {
         console.error(error)
     })
+    console.timeEnd("exec")
 }
 
 // Question 2
-QuestionTwo = async () => {
+class QueenAttack {
+    constructor(queen1, queen2) {
+        if (queen1[0] === queen2[0] && queen1[1] === queen2[1]) throw new Error("Queens cannot be at the same position")
+        if (queen1[0] >= 8 || queen1[0] < 0 || queen1[1] >= 8 || queen1[1] < 0) throw new Error("Queens cannot be outside the board")
+        if (queen2[0] >= 8 || queen2[0] < 0 || queen2[1] >= 8 || queen2[1] < 0) throw new Error("Queens cannot be outside the board")
+        this.queen1 = queen1
+        this.queen2 = queen2
+    }
+    canAttack() {
+        // If queen and the opponent are in the same row
+        if (this.queen1[1] === this.queen2[1]) return "Queens can attack because they are in the same row"
+        // If queen and the opponent are in the same column
+        if (this.queen1[0] === this.queen2[0]) return "Queens can attack because they are in the same column"
+        // If queen can attack the opponent diagonally
+        if (this.queen1[1] - this.queen2[1] === this.queen1[0] - this.queen2[0]) return "Queens can attack each other diagonally"
+        return "Queens cannot attack each other"
+    }
+}
 
+QuestionTwo = async () => {
+    const queen1 = [parseInt($("#qox").val()), parseInt($("#qoy").val())]
+    const queen2 = [parseInt($("#qtx").val()), parseInt($("#qty").val())]
+    const queens = new QueenAttack(queen1, queen2)
+    var div = $('<div> <br/> For Queen1 position: x = ' + queen1[0] + ' and y = ' + queen1[1] + '<br/> For Queen2 position: x = ' + queen2[0] + ' and y = ' + queen2[1] + '<br/>' + queens.canAttack() + '</div>');
+    $('.answer-container').append(div);
 }
